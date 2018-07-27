490e949467cf492f63c5912322e889cec896e697
ATS <ATS@192.168.2.6>
2018-07-27 10:40:08 +0530

    // Neha

--- src/main/java/com/ats/tril/repository/indent/GetIndentRepo.java
+++ src/main/java/com/ats/tril/repository/indent/GetIndentRepo.java
@@ -8,16 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ats.tril.model.indent.GetIndent;
+import com.ats.tril.model.indent.IndentReport;
+
 @Repository
 public interface GetIndentRepo extends JpaRepository<GetIndent, Integer> {
 
-	@Query(value=" SELECT indent.ind_m_id,indent.ind_m_no,indent.ind_m_date,indent.ind_isdev,indent.ind_ismonthly,indent.ind_m_type,indent.achd_id ,"
-			+ "indent.dept_id,indent.sub_dept_id, " + 
-			" m_category.cat_desc,indent.ind_m_status FROM indent,m_category WHERE indent.ind_m_date BETWEEN :fromDate AND :toDate " + 
-			" AND indent.cat_id=m_category.cat_id AND indent.ind_m_status IN (:status) " ,nativeQuery=true) 
-		
-		List<GetIndent> getIndent(@Param("fromDate") String fromDate,@Param("toDate") String toDate,
-				@Param("status") List<String> status);
-	
+	@Query(value = " SELECT indent.ind_m_id,indent.ind_m_no,indent.ind_m_date,indent.ind_isdev,indent.ind_ismonthly,indent.ind_m_type,indent.achd_id ,"
+			+ "indent.dept_id,indent.sub_dept_id, "
+			+ " m_category.cat_desc,indent.ind_m_status FROM indent,m_category WHERE indent.ind_m_date BETWEEN :fromDate AND :toDate "
+			+ " AND indent.cat_id=m_category.cat_id AND indent.ind_m_status IN (:status) ", nativeQuery = true)
+
+	List<GetIndent> getIndent(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
+			@Param("status") List<String> status);
 
 }
--- src/main/java/com/ats/tril/repository/indent/IndentRepository.java
+++ src/main/java/com/ats/tril/repository/indent/IndentRepository.java
@@ -11,23 +11,21 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ats.tril.model.indent.Indent;
 
 public interface IndentRepository extends JpaRepository<Indent, Integer> {
-	
+
 	Indent save(Indent indent);
-	
+
 	@Transactional
 	@Modifying
 	@Query(" UPDATE Indent  SET achdId=:achdId,deptId=:deptId,subDeptId=:subDeptId,indIsdev=:indIsdev,indIsmonthly=:indIsmonthly "
 			+ " WHERE indMId=:indMId ")
-		int updateIndentHeader(@Param("achdId") 
-		int achdId,@Param("deptId") int deptId,
-		@Param("subDeptId") 
-		int subDeptId,@Param("indIsdev") int indIsdev,
-		@Param("indIsmonthly") 
-		int indIsmonthly,@Param("indMId") int indMId);
+	int updateIndentHeader(@Param("achdId") int achdId, @Param("deptId") int deptId, @Param("subDeptId") int subDeptId,
+			@Param("indIsdev") int indIsdev, @Param("indIsmonthly") int indIsmonthly, @Param("indMId") int indMId);
 
 	@Transactional
 	@Modifying
 	@Query(" UPDATE Indent  SET ind_m_status=:status WHERE ind_m_id=:indMId ")
-	int updateStatus(@Param("indMId")int indMId,@Param("status") int status);
+	int updateStatus(@Param("indMId") int indMId, @Param("status") int status);
+
 	
+
 }
