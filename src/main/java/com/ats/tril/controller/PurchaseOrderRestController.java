62f036919cd323b93608d2e2f74791303255a183
ATS <ATS@192.168.2.6>
2018-07-27 12:39:02 +0530

    // Neha

--- src/main/java/com/ats/tril/controller/PurchaseOrderRestController.java
+++ src/main/java/com/ats/tril/controller/PurchaseOrderRestController.java
@@ -25,134 +25,119 @@ import com.ats.tril.repository.getpodetail.GetPODetailRepo;
 
 @RestController
 public class PurchaseOrderRestController {
-	
+
 	@Autowired
 	PoHeaderRepository poHeaderRepository;
-	
+
 	@Autowired
 	PoDetailRepository poDetailRepository;
-	 
+
 	@Autowired
 	GetPODetailRepo getPODetailRepo;
-	
+
 	@Autowired
 	GetPoHeaderListRepository getPoHeaderListRepository;
-	
+
 	@Autowired
 	GetPoDetailListRepository getPoDetailListRepository;
-	
-	
+
 	@RequestMapping(value = { "/getPODetailList" }, method = RequestMethod.POST)
 	public @ResponseBody List<GetPODetail> getPODetails(@RequestParam("poIdList") List<Integer> poIdList) {
 
-		
 		System.err.println("Inside getPODetailList ");
 		List<GetPODetail> poDetailList = new ArrayList<GetPODetail>();
 
 		try {
-			 
+
 			poDetailList = getPODetailRepo.getPoDetailsByPoIds(poIdList);
-			System.err.println("poDetailList  " +poDetailList.toString());
+			System.err.println("poDetailList  " + poDetailList.toString());
 
 		} catch (Exception e) {
-			
+
 			System.err.println("Exception /getPODetailList @PurchaseOrderRestControlle ");
 
 			e.printStackTrace();
-			 
+
 		}
 		return poDetailList;
 
 	}
 
-	
-	
-	//Sachin 12-07-2018
+	// Sachin 12-07-2018
 	@RequestMapping(value = { "/getPOHeaderList" }, method = RequestMethod.POST)
-	public @ResponseBody List<PoHeader> getPOHeaderList(@RequestParam("vendId") int vendId,@RequestParam("delStatus") int delStatus,
-			@RequestParam("statusList") List<Integer> statusList) {
+	public @ResponseBody List<PoHeader> getPOHeaderList(@RequestParam("vendId") int vendId,
+			@RequestParam("delStatus") int delStatus, @RequestParam("statusList") List<Integer> statusList) {
 
 		List<PoHeader> poHeaderList = new ArrayList<PoHeader>();
 
 		try {
 
-			 
 			poHeaderList = poHeaderRepository.findByVendIdAndDelStatusAndPoStatusIn(vendId, delStatus, statusList);
-				
 
 		} catch (Exception e) {
-			
+
 			System.err.println("Exception /getPOHeaderList @PurchaseOrderRestControlle ");
 
 			e.printStackTrace();
-			 
+
 		}
 		return poHeaderList;
 
 	}
 
-	
 	@RequestMapping(value = { "/savePoHeaderAndDetail" }, method = RequestMethod.POST)
-	public @ResponseBody PoHeader savePoHeaderAndDetail(@RequestBody  PoHeader  poHeader) {
+	public @ResponseBody PoHeader savePoHeaderAndDetail(@RequestBody PoHeader poHeader) {
 
 		PoHeader save = new PoHeader();
 
 		try {
 
-			 
 			save = poHeaderRepository.save(poHeader);
-				
-				for(int i=0 ; i<poHeader.getPoDetailList().size();i++)
-				{
-					poHeader.getPoDetailList().get(i).setPoId(save.getPoId());
-					poHeader.getPoDetailList().get(i).setVendId(save.getVendId());
-				}
-				
-			List<PoDetail> poDetailList	= poDetailRepository.saveAll(poHeader.getPoDetailList());
+
+			for (int i = 0; i < poHeader.getPoDetailList().size(); i++) {
+				poHeader.getPoDetailList().get(i).setPoId(save.getPoId());
+				poHeader.getPoDetailList().get(i).setVendId(save.getVendId());
+			}
+
+			List<PoDetail> poDetailList = poDetailRepository.saveAll(poHeader.getPoDetailList());
 			save.setPoDetailList(poDetailList);
-			 
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			 
+
 		}
 		return save;
 
 	}
-	
-	
+
 	@RequestMapping(value = { "/deletePo" }, method = RequestMethod.POST)
-	public @ResponseBody ErrorMessage deletePo(@RequestParam("poId")  int  poId) {
+	public @ResponseBody ErrorMessage deletePo(@RequestParam("poId") int poId) {
 
 		ErrorMessage errorMessage = new ErrorMessage();
 
 		try {
 
-			 
 			int delete = poHeaderRepository.delete(poId);
-				 if(delete!=0)
-				 {
-					 errorMessage.setError(false);
-					 errorMessage.setMessage("deleted");
-				 }
-				 else
-				 {
-					 errorMessage.setError(true);
-					 errorMessage.setMessage("failed");
-				 }
+			if (delete != 0) {
+				errorMessage.setError(false);
+				errorMessage.setMessage("deleted");
+			} else {
+				errorMessage.setError(true);
+				errorMessage.setMessage("failed");
+			}
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			 errorMessage.setError(true);
-			 errorMessage.setMessage("failed");
-			 
+			errorMessage.setError(true);
+			errorMessage.setMessage("failed");
+
 		}
 		return errorMessage;
 
 	}
-	
+
 	@RequestMapping(value = { "/getPoHeaderListBetweenDate" }, method = RequestMethod.POST)
 	public @ResponseBody List<GetPoHeaderList> getPoHeaderListBetweenDate(@RequestParam("fromDate") String fromDate,
 			@RequestParam("toDate") String toDate) {
@@ -161,39 +146,84 @@ public class PurchaseOrderRestController {
 
 		try {
 
-			 
-			list = getPoHeaderListRepository.getPoHeaderListBetweenDate(fromDate,toDate);
-				
-			  
+			list = getPoHeaderListRepository.getPoHeaderListBetweenDate(fromDate, toDate);
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			 
+
 		}
 		return list;
 
 	}
-	
-	@RequestMapping(value = { "/getPoHeaderAndDetailByHeaderId" }, method = RequestMethod.POST)
-	public @ResponseBody  GetPoHeaderList  getPoHeaderAndDetailByHeaderId(@RequestParam("poId") int poId ) {
 
-		 GetPoHeaderList  getPoHeaderList = new  GetPoHeaderList ();
+	@RequestMapping(value = { "/getPoHeaderAndDetailByHeaderId" }, method = RequestMethod.POST)
+	public @ResponseBody GetPoHeaderList getPoHeaderAndDetailByHeaderId(@RequestParam("poId") int poId) {
+
+		GetPoHeaderList getPoHeaderList = new GetPoHeaderList();
 
 		try {
- 
-					getPoHeaderList = getPoHeaderListRepository.getPoHeaderAndDetailByHeaderId(poId);
-					
-					List<GetPoDetailList> poDetailList = getPoDetailListRepository.getDetail(poId);
-					getPoHeaderList.setPoDetailList(poDetailList);
-			  
+
+			getPoHeaderList = getPoHeaderListRepository.getPoHeaderAndDetailByHeaderId(poId);
+
+			List<GetPoDetailList> poDetailList = getPoDetailListRepository.getDetail(poId);
+			getPoHeaderList.setPoDetailList(poDetailList);
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
-			 
+
 		}
 		return getPoHeaderList;
+
+	}
+
+	@RequestMapping(value = { "/getPoHeaderListReport" }, method = RequestMethod.POST)
+	public @ResponseBody List<GetPoHeaderList> getPoHeaderListReport(@RequestParam("fromDate") String fromDate,
+			@RequestParam("toDate") String toDate, @RequestParam("vendorIdList") List<Integer> vendorIdList,
+			@RequestParam("poTypeList") List<Integer> poTypeList, @RequestParam("poStatus") List<Integer> poStatus) {
+
+		List<GetPoHeaderList> list = new ArrayList<GetPoHeaderList>();
+
+		try {
+			if (vendorIdList.contains(0) && poTypeList.contains(0) && poStatus.contains(0)) {
+
+				list = getPoHeaderListRepository.getPoHeaderListBetweenDate(fromDate, toDate);
+			} else if (vendorIdList.contains(0) && poTypeList.contains(0) && !poStatus.contains(0)) {
+
+				list = getPoHeaderListRepository.getPoHeaderByStatus(fromDate, toDate, poStatus);
+			} else if (vendorIdList.contains(0) && !poTypeList.contains(0) && !poStatus.contains(0)) {
+
+				list = getPoHeaderListRepository.getPoHeaderByStatusAndPoTypeList(fromDate, toDate, poStatus,
+						poTypeList);
+			} else if (!vendorIdList.contains(0) && poTypeList.contains(0) && !poStatus.contains(0)) {
+
+				list = getPoHeaderListRepository.getPoHeaderByVendorAndPoType(fromDate, toDate, vendorIdList,
+						poTypeList);
+			}
+
+			else if (!vendorIdList.contains(0) && poTypeList.contains(0) && poStatus.contains(0)) {
+
+				list = getPoHeaderListRepository.getPoHeaderByVendor(fromDate, toDate, vendorIdList);
+			}
+
+			else if (!vendorIdList.contains(0) && !poTypeList.contains(0) && poStatus.contains(0)) {
+
+				list = getPoHeaderListRepository.getPoHeaderByVendorAndPoTypeList(fromDate, toDate, vendorIdList,
+						poTypeList);
+			}
+
+			else {
+
+				list = getPoHeaderListRepository.getPoHeaderByStatusAndPoTypeAndVendorList(fromDate, toDate, poStatus,
+						poTypeList, vendorIdList);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+		return list;
 
 	}
 
--- src/main/java/com/ats/tril/repository/GetPoHeaderListRepository.java
+++ src/main/java/com/ats/tril/repository/GetPoHeaderListRepository.java
@@ -5,18 +5,44 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
- 
-import com.ats.tril.model.GetPoHeaderList; 
 
-public interface GetPoHeaderListRepository extends JpaRepository<GetPoHeaderList, Integer>{
+import com.ats.tril.model.GetPoHeaderList;
 
-	
+public interface GetPoHeaderListRepository extends JpaRepository<GetPoHeaderList, Integer> {
+
 	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id", nativeQuery = true)
-	List<GetPoHeaderList> getPoHeaderListBetweenDate(@Param("fromDate")String fromDate,@Param("toDate") String toDate);
+	List<GetPoHeaderList> getPoHeaderListBetweenDate(@Param("fromDate") String fromDate,
+			@Param("toDate") String toDate);
 
 	@Query(value = "select p.*,v.vendor_name from po_header p,m_vendor v where po_id=:poId and del_status=1 and v.vendor_id =p.vend_id", nativeQuery = true)
-	GetPoHeaderList getPoHeaderAndDetailByHeaderId(@Param("poId")int poId);
-	
-	
+	GetPoHeaderList getPoHeaderAndDetailByHeaderId(@Param("poId") int poId);
+
+	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id AND p.po_status IN(:poStatus)", nativeQuery = true)
+	List<GetPoHeaderList> getPoHeaderByStatus(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
+			@Param("poStatus") List<Integer> poStatus);
+
+	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id AND p.po_status IN(:poStatus)  AND p.po_type IN(:poTypeList)", nativeQuery = true)
+	List<GetPoHeaderList> getPoHeaderByStatusAndPoTypeList(@Param("fromDate") String fromDate,
+			@Param("toDate") String toDate, @Param("poStatus") List<Integer> poStatus,
+			@Param("poTypeList") List<Integer> poTypeList);
+
+	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id AND p.po_status IN(:poStatus)  AND p.po_type IN(:poTypeList)  AND p.vend_id IN(:vendorIdList)", nativeQuery = true)
+	List<GetPoHeaderList> getPoHeaderByStatusAndPoTypeAndVendorList(@Param("fromDate") String fromDate,
+			@Param("toDate") String toDate, @Param("poStatus") List<Integer> poStatus,
+			@Param("poTypeList") List<Integer> poTypeList, @Param("vendorIdList") List<Integer> vendorIdList);
+
+	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id   AND p.po_type IN(:poTypeList)  AND p.vend_id IN(:vendorIdList)", nativeQuery = true)
+	List<GetPoHeaderList> getPoHeaderByVendorAndPoType(@Param("fromDate") String fromDate,
+			@Param("toDate") String toDate, @Param("poTypeList") List<Integer> poTypeList,
+			@Param("vendorIdList") List<Integer> vendorIdList);
+
+	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id   AND  p.vend_id IN(:vendorIdList)", nativeQuery = true)
+	List<GetPoHeaderList> getPoHeaderByVendor(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
+			@Param("vendorIdList") List<Integer> vendorIdList);
+
+	@Query(value = " select p.*,v.vendor_name from po_header p,m_vendor v where p.po_date between :fromDate and :toDate and del_status=1 and v.vendor_id =p.vend_id   AND p.po_type IN(:poTypeList)  AND p.vend_id IN(:vendorIdList)", nativeQuery = true)
+	List<GetPoHeaderList> getPoHeaderByVendorAndPoTypeList(@Param("fromDate") String fromDate,
+			@Param("toDate") String toDate, @Param("poTypeList") List<Integer> poTypeList,
+			@Param("vendorIdList") List<Integer> vendorIdList);
 
 }
