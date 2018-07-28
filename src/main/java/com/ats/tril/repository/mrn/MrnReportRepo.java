d84bbbef4487a3a8b94376f0a2bd95f02bd95158
Aaryatech <code@atsinfosoft.com>
2018-07-28 14:32:05 +0530

    mrnreport//Sac

--- src/main/java/com/ats/tril/repository/mrn/MrnReportRepo.java
+++ src/main/java/com/ats/tril/repository/mrn/MrnReportRepo.java
@@ -19,10 +19,12 @@ public interface MrnReportRepo  extends JpaRepository<MrnReport, Integer>{
 	List<MrnReport> getMrnReport( @Param("vendorIdList") List<String> vendorIdList,@Param("grnTypeList") List<String> grnTypeList,
 			@Param("statusList") List<String> statusList,@Param("fromDate") String fromDate, @Param("toDate") String toDate);
 
-	@Query(value = " SELECT h.mrn_id,h.mrn_no,h.mrn_type,h.mrn_date,h.vendor_id,v.vendor_name,h.gate_entry_no,h.gate_entry_date,h.doc_no," + 
-			"			  h.doc_date,h.bill_no,h.bill_date,h.transport,h.lr_no,h.lr_date,h.remark1,h.remark2,h.user_id,h.mrn_status,h.del_status "+ 
-			"			 FROM t_mrn_header h,m_vendor v WHERE h.vendor_id=v.vendor_id AND h.mrn_type " + 
-			" IN(:grnTypeList) AND h.mrn_status IN(:statusList) AND h.del_status=1 AND h.mrn_date BETWEEN :fromDate AND :toDate"
+	@Query(value = " SELECT h.mrn_no,h.mrn_type,h.mrn_date,v.vendor_name,h.user_id,h.mrn_status, \" + \n" + 
+			"			\" detail.*, item.item_code,item.item_desc, (pod.landing_cost / pod.item_qty) as landing_rate, pod.item_rate AS item_rate \" + \n" + 
+			"			\" FROM t_mrn_header h,m_vendor v, t_mrn_detail detail,m_item item,po_detail pod \" + \n" + 
+			"			\" WHERE  v.vendor_id=h.vendor_id AND h.mrn_type IN(:grnTypeList) AND h.mrn_status IN(:statusList) \"+\n" + 
+			"			 \" AND h.del_status=1 AND h.mrn_date BETWEEN :fromDate AND :toDate \" +\n" + 
+			"			\" AND h.mrn_id=detail.mrn_id AND detail.po_detail_id=pod.po_detail_id AND detail.item_id=item.item_id"
 			+ " ", nativeQuery = true)
 	List<MrnReport> getMrnReportAllVendor(@Param("fromDate") String fromDate, @Param("toDate") String toDate,@Param("grnTypeList") List<String> grnTypeList, @Param("statusList") List<String> statusList);
 
