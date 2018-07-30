6d73749337613c4a3c5603e2f91d255e0d83f865
Viplav Thoke <viplavthoke@gmail.com>
2018-07-30 16:03:50 +0530

    /Akshay

--- src/main/java/com/ats/tril/repository/GetItemRepository.java
+++ src/main/java/com/ats/tril/repository/GetItemRepository.java
@@ -11,7 +11,7 @@ import com.ats.tril.model.GetItem;
 public interface GetItemRepository extends JpaRepository<GetItem, Integer> {
 
 	@Query(value = "select m.*, c.cat_desc, g.grp_code, sg.subgrp_desc from m_item m, m_category c, m_item_group g, m_item_subgroup sg "
-			+ " where  c.cat_id = m.cat_id and g.grp_id = m.grp_id and sg.subgrp_id = m.sub_grp_id ", nativeQuery = true)
+			+ " where  c.cat_id = m.cat_id and g.grp_id = m.grp_id and sg.subgrp_id = m.sub_grp_id and m.is_used = 1", nativeQuery = true)
 	List<GetItem> getAllItems();
 
 	@Query(value = "select m.*, c.cat_desc, g.grp_code, sg.subgrp_desc from m_item m, m_category c, m_item_group g, m_item_subgroup sg "
@@ -19,11 +19,11 @@ public interface GetItemRepository extends JpaRepository<GetItem, Integer> {
 	GetItem getItemByItemId(@Param("itemId") int itemId);
 
 	@Query(value = "select m.*, c.cat_desc, g.grp_code, sg.subgrp_desc from m_item m, m_category c, m_item_group g, m_item_subgroup sg "
-			+ " where  c.cat_id = m.cat_id and g.grp_id = m.grp_id and sg.subgrp_id = m.sub_grp_id and g.grp_id=:groupId", nativeQuery = true)
+			+ " where  c.cat_id = m.cat_id and g.grp_id = m.grp_id and sg.subgrp_id = m.sub_grp_id and g.grp_id=:groupId and m.is_used = 1", nativeQuery = true)
 	List<GetItem> itemListByGroupId(@Param("groupId") int groupId);
 
 	@Query(value = "select m.*, c.cat_desc, g.grp_code, sg.subgrp_desc from m_item m, m_category c, m_item_group g, m_item_subgroup sg "
-			+ " where  c.cat_id = m.cat_id and g.grp_id = m.grp_id and sg.subgrp_id = m.sub_grp_id and m.cat_id=:catId", nativeQuery = true)
+			+ " where  c.cat_id = m.cat_id and g.grp_id = m.grp_id and sg.subgrp_id = m.sub_grp_id and m.cat_id=:catId and m.is_used = 1", nativeQuery = true)
 	List<GetItem> itemListByCatId(@Param("catId") int catId);
 
 }
