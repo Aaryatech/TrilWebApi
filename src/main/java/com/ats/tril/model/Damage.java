51f34034e11db36abaf9e505089acb2180ab97fa
Viplav Thoke <viplavthoke@gmail.com>
2018-08-13 17:47:08 +0530

    Damage //Akshay

---
+++ src/main/java/com/ats/tril/model/Damage.java
@@ -0,0 +1,101 @@
+package com.ats.tril.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_damage")
+public class Damage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "damage_id")
+	private int damageId;
+	
+	@Column(name = "date")
+	private String date;
+
+	@Column(name = "item_id")
+	private int itemId;
+
+	@Column(name = "qty")
+	private int qty;
+
+	@Column(name = "value")
+	private float value;
+	
+	@Column(name = "reason")
+	private String reason;
+	
+	@Column(name = "del_status")
+	private int delStatus;
+
+	public int getDamageId() {
+		return damageId;
+	}
+
+	public void setDamageId(int damageId) {
+		this.damageId = damageId;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public float getValue() {
+		return value;
+	}
+
+	public void setValue(float value) {
+		this.value = value;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public int getDelStatus() {
+		return delStatus;
+	}
+
+	public void setDelStatus(int delStatus) {
+		this.delStatus = delStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Damage [damageId=" + damageId + ", date=" + date + ", itemId=" + itemId + ", qty=" + qty + ", value="
+				+ value + ", reason=" + reason + ", delStatus=" + delStatus + "]";
+	}
+	
+	
+
+}
