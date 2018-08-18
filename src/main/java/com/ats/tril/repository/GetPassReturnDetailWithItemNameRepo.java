b5753376bf5cb8400bc8ce3ce43496ddcca8c8e2
Viplav Thoke <viplavthoke@gmail.com>
2018-08-18 15:41:56 +0530

    /Akshay

--- src/main/java/com/ats/tril/repository/GetPassReturnDetailWithItemNameRepo.java
+++ src/main/java/com/ats/tril/repository/GetPassReturnDetailWithItemNameRepo.java
@@ -10,7 +10,7 @@ import com.ats.tril.model.GetPassReturnDetailWithItemName;
 
 public interface GetPassReturnDetailWithItemNameRepo extends JpaRepository<GetPassReturnDetailWithItemName, Integer>{
 
-	@Query(value = "select gd.*,i.item_code from t_getpass_return_detail gd,m_item i where gd.return_id=:returnId and gd.is_used=1 and gd.gp_item_id = i.item_id ", nativeQuery = true)
+	@Query(value = "select gd.*,CONCAT(i.item_code, '-', i.item_desc) as item_code from t_getpass_return_detail gd,m_item i where gd.return_id=:returnId and gd.is_used=1 and gd.gp_item_id = i.item_id ", nativeQuery = true)
  	List<GetPassReturnDetailWithItemName> findByReturnId(@Param("returnId") int returnId);
 	
 	
