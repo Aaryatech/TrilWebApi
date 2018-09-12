1a07394697534558942862a72e825eb774a8a685
Viplav Thoke <viplavthoke@gmail.com>
2018-09-12 15:23:22 +0530

    /Akshay

--- src/main/java/com/ats/tril/repository/ItemQtyWithRecieptNoRepository.java
+++ src/main/java/com/ats/tril/repository/ItemQtyWithRecieptNoRepository.java
@@ -14,13 +14,13 @@ public interface ItemQtyWithRecieptNoRepository extends JpaRepository<ItemQtyWit
 	@Query(value="Select t_mrn_detail.mrn_detail_id as id,t_mrn_header.bill_date as date,t_mrn_header.mrn_no as recept_no,"
 			+ "t_mrn_detail.approve_qty as qty FROM t_mrn_detail, t_mrn_header where t_mrn_header.mrn_date =:date "
 			+ "AND t_mrn_header.mrn_id=t_mrn_detail.mrn_id AND t_mrn_detail.item_id=:itemId and t_mrn_header.del_status=1 "
-			+ "and t_mrn_detail.del_status=1 and t_mrn_detail.mrn_detail_status>0",nativeQuery=true)
+			+ "and t_mrn_detail.del_status=1 and t_mrn_detail.mrn_detail_status=4",nativeQuery=true)
 	List<ItemQtyWithRecieptNo> getMrnDataByDate(@Param("date") String date,@Param("itemId") int itemId);
 
 	@Query(value="Select item_issue_detail.issue_detail_id as id, item_issue_header.issue_date as date, "
 			+ "item_issue_header.issue_no as recept_no, item_issue_detail.item_issue_qty as qty FROM item_issue_detail, item_issue_header "
 			+ "where item_issue_header.issue_date =:date AND item_issue_header.issue_id=item_issue_detail.issue_id "
-			+ "AND item_issue_detail.item_id=:itemId and item_issue_header.delete_status=1 and item_issue_detail.del_status=1 ",nativeQuery=true)
+			+ "AND item_issue_detail.item_id=:itemId and item_issue_header.delete_status=1 and item_issue_detail.del_status=1 AND item_issue_detail.status = 2",nativeQuery=true)
 	List<ItemQtyWithRecieptNo> getIssueDataByDate(@Param("date") String date,@Param("itemId") int itemId);
 
 	@Query(value="Select t_gatepass_detail.gp_detail_id as id, t_gatepass_header.gp_date as date, t_gatepass_header.gp_no as recept_no, "
